5bca36bb932d630e957e25c335548621d1aec689
chcarrie <ppinkywei@gmail.com>
2025-08-20 20:12:17 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  base: process.env.NODE_ENV === 'production' ? '/vite-milkproject/' : '/',
+  plugins: [vue()],
+  base: '/vite-milkproject/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
